1585fd595a15dab827ea537ada778e1dc35834b9
jun_ <gurwns232@naver.com>
2024-03-11 11:12:35 +0900

    feat: home screen 생성

--- api.js
+++ api.js
@@ -3,14 +3,14 @@ import sanityClient from './sanity'
 let sanityQuery = (query, params) => sanityClient.fetch(query, params)
 
 export const getFeaturedRestaurants = () => {
-  sanityQuery(`
+  return sanityQuery(`
     *[_type=='featured']{
       ...,
       dishes[]->{
         name
       }
     }
-  `)
+    `)
 }
 
 export const getCategories = () => {
