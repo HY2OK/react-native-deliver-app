fa596242b39794beb17fb6ddbdad983ddea5a298
jun_ <gurwns232@naver.com>
2024-03-11 11:56:36 +0900

    feat: Restaurant Screen 생성

--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,18 +1,44 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigation, useRoute } from '@react-navigation/native'
+import { useDispatch } from 'react-redux'
+import { setRestaurant } from '../store/restaurantSlice'
 
-const RestaurantScreen = ({
-  id,
-  imgUrl,
-  title,
-  rating,
-  genre,
-  address,
-  shortDescription,
-  dishes,
-  long,
-  lat,
-}) => {
+const RestaurantScreen = () => {
+  const {
+    params: {
+      id,
+      imgUrl,
+      title,
+      rating,
+      genre,
+      address,
+      shortDescription,
+      dishes,
+      long,
+      lat,
+    },
+  } = useRoute()
+  const navigation = useNavigation()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(
+      setRestaurant({
+        id,
+        imgUrl,
+        title,
+        rating,
+        genre,
+        address,
+        short_description: shortDescription,
+        dishes,
+        long,
+        lat,
+      }),
+    )
+  }, [])
+
   return (
     <View>
       <Text>RestaurantScreen</Text>
