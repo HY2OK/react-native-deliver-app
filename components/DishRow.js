c0cdff8dcaf457b564378c71734ef188739fc892
jun_ <gurwns232@naver.com>
2024-03-12 14:29:03 +0900

    feat: DishRow 데이터 생성하기 (redux action, selector함수 사용)

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,7 +1,32 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+  addToBasket,
+  clearBasket,
+  removeFromBasket,
+  selectBasketItemsWithId,
+} from '../store/basketSlice'
 
-const DishRow = () => {
+const DishRow = ({ id, name, description, price, image }) => {
+  const [isPressed, setIsPressed] = useState(false)
+  const items = useSelector((state) => selectBasketItemsWithId(state, id))
+  const dispatch = useDispatch()
+
+  const addItemToBasket = () => {
+    dispatch(addToBasket({ id, name, description, price, image }))
+  }
+
+  const removeItemFromBasket = () => {
+    if (!items.length > 0) return
+
+    dispatch(removeFromBasket({ id }))
+  }
+
+  const clearItemsFromBasket = () => {
+    dispatch(clearBasket())
+  }
+
   return (
     <View>
       <Text>DishRow</Text>
