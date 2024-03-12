16da8b8cfb956b2536aa4d769fb169871e2e2994
jun_ <gurwns232@naver.com>
2024-03-12 15:11:23 +0900

    feat: Basket Screen 생성

--- screens/BasketScreen.js
+++ screens/BasketScreen.js
@@ -1,7 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectRestaurant } from '../store/restaurantSlice'
+import { selectBasketItems, selectBasketTotal } from '../store/basketSlice'
 
 const BasketScreen = () => {
+  const navigation = useNavigation()
+  const restaurant = useSelector(selectRestaurant)
+  const items = useSelector(selectBasketItems)
+  const basketTotal = useSelector(selectBasketTotal)
+  const [groupedItemsInBasket, setGroupedItemsInBasket] = useState({})
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const groupedItems = items.reduce((results, item) => {
+      ;(results[item.id] = results[item.id] || []).push(item)
+      return results
+    }, {})
+    setGroupedItemsInBasket(groupedItems)
+  }, [items])
+
   return (
     <View>
       <Text>BasketScreen</Text>
