f2e4899e6d1bcf00efc1c31c1c384fc126a82675
jun_ <gurwns232@naver.com>
2024-03-12 16:07:10 +0900

    feat: Preparing Order Screen 생성

--- screens/DeliveryScreen.js
+++ screens/DeliveryScreen.js
@@ -1,11 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const DeliveryScreen = () => {
   return (
-    <View>
-      <Text>DeliveryScreen</Text>
-    </View>
+    <SafeAreaView>
+      <View className="flex-row items-center pb-3 mx-4 space-x-2">
+        <View className="flex-1">
+          <Text className="text-xs font-bold text-gray-400">배송 앱</Text>
+          <Text className="text=xl font-bold">현재 위치</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
